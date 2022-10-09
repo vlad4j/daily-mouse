1b8fd0ee75b977dfb28c3fdf3c5b573c0838cd19
Vladislav <vladislav.kovchug@gmail.com>
2022-10-09 18:35:20 +0200

    Add schedule task.

--- lib/mouse-stack.ts
+++ lib/mouse-stack.ts
@@ -1,19 +1,36 @@
 import {Duration, Stack, StackProps} from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { Construct } from 'constructs';
-import {Runtime} from "aws-cdk-lib/aws-lambda";
+import {Runtime} from 'aws-cdk-lib/aws-lambda';
+import {Construct} from 'constructs';
 import {LambdaCode} from "./helper/codeHelper";
 import {PolicyDocument, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {Rule, Schedule} from "aws-cdk-lib/aws-events";
+import {LambdaFunction} from "aws-cdk-lib/aws-events-targets";
 
 export class MouseStack extends Stack {
   private nodeJSLayer: lambda.LayerVersion;
+  private mouseLambda: lambda.IFunction;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     this.nodeJSLayer = this.createModeJsLayer();
 
-    this.createMouseLambda();
+    this.mouseLambda = this.createMouseLambda();
+
+    this.createMousePostRule();
+  }
+
+  private createMousePostRule() {
+    const lambdaTarget = new LambdaFunction(this.mouseLambda, {
+      maxEventAge: Duration.hours(2),
+      retryAttempts: 1
+    });
+
+    new Rule(this, 'ScheduleRule', {
+      schedule: Schedule.cron({minute: '0', hour: '9'}),
+      targets: [lambdaTarget],
+    });
   }
 
   private createModeJsLayer() {
